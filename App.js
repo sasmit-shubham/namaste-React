21cbec7e129ec9e3200d9a21989cfe452bd55671
Shubham Kumar <msm19b044@iiitdm.ac.in>
2023-07-09 16:53:28 +0530

    first episode of the namaste js

---
+++ App.js
@@ -0,0 +1,4 @@
+const header = React.createElement("div",{id:"parent"},[React.createElement("h1",{},"This is h1 tag"),
+React.createElement("h2",{},"This is h2 tag")]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
